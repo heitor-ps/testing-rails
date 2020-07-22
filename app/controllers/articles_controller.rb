392a278a224b8fad84935d021a2798b6640f7db7
heitor-ps <heytorpg7@gmail.com>
2020-07-22 12:32:25 -0300

    novas funcoes adicionadas

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,20 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def new
 		@article = Article.new
 	end
@@ -21,6 +35,13 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+	
 	private 
 	def article_params
 		params.require(:article).permit(:title,:text)
